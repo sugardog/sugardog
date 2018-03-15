dcf6db5a521fb1714b9790ffd86fcb9711f14d1a
JohnnyMJO <i.makoto.gm@gmail.com>
2018-03-15 12:23:53 +0000

    User実装途中 seed-fuのｃｄが未完成

---
+++ db/fixtures/artist.rb
@@ -0,0 +1,95 @@
+Artist.seed do |s|
+	s.id = 1
+	s.artist_name = "aiko"
+	s.artist_kana_name = "アイコ"
+	s.introduction = "出身地は大阪。1975年11月22日生まれの蠍座。"
+	s.image = Rails.root.join("db/fixtures/images/aiko_favorite.jpg").open
+end
+
+Artist.seed do |s|
+	s.id = 2
+ 	s.artist_name = "星野源"
+ 	s.artist_kana_name = "ホシノゲン"
+ 	s.introduction = "1981年1月28日生まれ、埼玉県蕨市出身のシンガー・ソングライター／俳優／作家。"
+ 	s.image = Rails.root.join("db/fixtures/images/hosinogen_favorite.jpg").open
+end
+
+Artist.seed do |s|
+ 	s.id = 3
+ 	s.artist_name = "BIGBANG"
+ 	s.artist_kana_name = "ビッグバン"
+ 	s.introduction = "韓国出身の男性ダンス＆ヴォーカル・グループ。"
+ 	s.image = Rails.root.join("db/fixtures/images/bigbang_favorite.jpg").open
+end
+
+Artist.seed do |s|
+ 	s.id = 4
+ 	s.artist_name = "lynch."
+ 	s.artist_kana_name = "リンチ"
+ 	s.introduction = "愛知県名古屋市出身のロック・バンド。"
+ 	s.image = Rails.root.join("db/fixtures/images/lynch_favorite.jpg").open
+end
+
+Artist.seed do |s|
+ 	s.id = 5
+ 	s.artist_name = "菅田将暉"
+ 	s.artist_kana_name = "スダマサキ"
+ 	s.introduction = "1993年2月21日生まれ、大阪府箕面市出身の俳優／歌手。"
+ 	s.image = Rails.root.join("db/fixtures/images/suda_favorite.jpg").open
+end
+
+Artist.seed do |s|
+ 	s.id = 6
+ 	s.artist_name = "東京事変"
+ 	s.artist_kana_name = "トウキョウジヘン"
+ 	s.introduction = "椎名林檎を中心に結成された5人組のロック・バンド。"
+ 	s.image = Rails.root.join("db/fixtures/images/tokyojihen_favorite.jpg").open
+end
+
+Artist.seed do |s|
+ 	s.id = 7
+ 	s.artist_name = "モーニング娘。"
+ 	s.artist_kana_name = "モーニングムスメ"
+ 	s.introduction = "日本の女性ダンス＆ヴォーカル・グループ。"
+ 	s.image = Rails.root.join("db/fixtures/images/momus_favorite.jpg").open
+end
+
+Artist.seed do |s|
+ 	s.id = 8
+ 	s.artist_name = "Herbert von Karajan"
+ 	s.artist_kana_name = "ヘルベルト・フォン・カラヤン"
+ 	s.introduction = "指揮者。1908年オーストリア、ザルツブルク生まれ。"
+ 	s.image = Rails.root.join("db/fixtures/images/karayan_favorite.jpg").open
+end
+
+Artist.seed do |s|
+ 	s.id = 9
+ 	s.artist_name = "モーニング娘。20th"
+ 	s.artist_kana_name = "モーニングムスメ２０"
+ 	s.introduction = ""
+ 	s.image = Rails.root.join("db/fixtures/images/momus3.jpg").open
+end
+
+ Artist.seed do |s|
+ 	s.id = 10
+ 	s.artist_name = "西野カナ"
+ 	s.artist_kana_name = "ニシノカナ"
+ 	s.introduction = "1989年3月18日生まれ、三重県松阪市出身の女性シンガー。"
+ 	s.image = Rails.root.join("db/fixtures/images/nishino_favorite.jpg").open
+end
+
+Artist.seed do |s|
+ 	s.id = 11
+ 	s.artist_name = "EGOIST"
+ 	s.artist_kana_name = "エゴイスト"
+ 	s.introduction = "supercellのryoプロデュースによる音楽ユニット。"
+ 	s.image = Rails.root.join("db/fixtures/images/egoist_favorite.jpg").open
+end
+
+Artist.seed do |s|
+ 	s.id = 12
+ 	s.artist_name = "ONE OK ROCK"
+ 	s.artist_kana_name = "ワンオクロック"
+ 	s.introduction = "日本のロック・バンド。メンバーはTAKA（vo）、TORU（g）、RYOTA（b）、TOMOYA（ds）の4名。"
+ 	s.image = Rails.root.join("db/fixtures/images/one_favorite.jpg").open
+end
