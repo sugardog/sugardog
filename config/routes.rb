c8b02bf806482a3169a9886fd86970bacd3616ff
ShinsukeJPN <pgcxw275@gmail.com>
2018-03-16 05:58:06 +0000

    genre_added

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,34 @@
 Rails.application.routes.draw do
-  devise_for :users
+  	devise_for :users
 
-  devise_for :admins
+  	devise_for :admins
 
-get 'cds/search' => 'cds#search'
-get 'cd_carts/:id/select' => 'cd_carts#select'
+	get 'cds/search' => 'cds#search'
+	get 'cd_carts/:id/select' => 'cd_carts#select'
 
-resources :users do
-	resources :histories, only: [:index]
-end
-resources :deliveries
-resources :cds do
-	resources :discs do
-		resources :songs
+	resources :users do
+		resources :histories, only: [:index]
 	end
-end
-resources :carts
-resources :histories
-resources :genres, except: [:show]
-resources :pop_images, except: [:edit,:update,:show]
-resources :songs
-resources :artists do
-	resource :favorites, only: [:create, :destroy]
-end
-resources :campaigns, except: [:show]
-resources :prefectures, except: [:show]
-resource :singers, except: [:show, :index, :edit]
-resources :admins
-resources :cd_carts
-resource :reviews, except: [:show]
-resources :cd_histories, only: [:show]
-resources :cd_genre, only: [:show]
+	resources :deliveries
+	resources :cds do
+		resources :discs do
+			resources :songs
+		end
+	end
+	resources :carts
+	resources :histories
+	resources :genres, except: [:new, :show]
+	resources :pop_images, except: [:edit,:update,:show]
+	resources :songs
+	resources :artists do
+		resource :favorites, only: [:create, :destroy]
+	end
+	resources :campaigns, except: [:show]
+	resources :prefectures, except: [:show]
+	resource :singers, except: [:show, :index, :edit]
+	resources :admins
+	resources :cd_carts
+	resource :reviews, except: [:show]
+	resources :cd_histories, only: [:show]
+	resources :cd_genre, only: [:show]
 end
