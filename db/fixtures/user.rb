f68839f94e37e7184325bd263ebe60a335ec980d
JohnnyMJO <i.makoto.gm@gmail.com>
2018-03-16 16:34:00 +0000

    途中保存

--- db/fixtures/user.rb
+++ db/fixtures/user.rb
@@ -26,8 +26,127 @@ User.seed do |s|
 	s.tel = "0358658422"
 	#s.prefecture_id = 
 	s.address = "港区西麻布1-1-1"
-	s.password = "888888"
+	s.password = "012345"
 	s.nickname = "hotei"
+	#s.cart_id = 
+	#s.doft_destroyed_at = 
+end
+
+User.seed do |s|
+	s.id = 3
+	s.first_name = "斉藤"
+	s.last_name = "和義"
+	s.kana_first_name = "サイトウ"
+	s.kana_last_name = "カズヨシ"
+	s.email = "sample3@sample3.com"
+	s.zipcode = "0321011"
+	s.tel = "0358658422"
+	#s.prefecture_id = 
+	s.address = "目黒区中目黒1-1-1"
+	s.password = "333333"
+	s.nickname = "せっちゃん"
+	#s.cart_id = 
+	#s.doft_destroyed_at = 
+end
+
+User.seed do |s|
+	s.id = 4
+	s.first_name = "水原"
+	s.last_name = "希子"
+	s.kana_first_name = "ミズハラ"
+	s.kana_last_name = "キコ"
+	s.email = "ika@gmail.com"
+	s.zipcode = "0987654"
+	s.tel = "0358658422"
+	#s.prefecture_id = 
+	s.address = "港区六本木1-1-1"
+	s.password = "888888"
+	s.nickname = "きこちゃん"
+	#s.cart_id = 
+	#s.doft_destroyed_at = 
+end
+
+User.seed do |s|
+	s.id = 5
+	s.first_name = "綾瀬"
+	s.last_name = "はるか"
+	s.kana_first_name = "アヤセ"
+	s.kana_last_name = "ハルカ"
+	s.email = "choco@gmail.com"
+	s.zipcode = "1432222"
+	s.tel = "09011112222"
+	#s.prefecture_id = 
+	s.address = "津市栗間町屋町1-1-1"
+	s.password = "888888"
+	s.nickname = "haruka"
+	#s.cart_id = 
+	#s.doft_destroyed_at = 
+end
+
+User.seed do |s|
+	s.id = 6
+	s.first_name = "山本"
+	s.last_name = "美月"
+	s.kana_first_name = "ヤマモト"
+	s.kana_last_name = "ミツキ"
+	s.email = "amerikandog@gmail.com"
+	s.zipcode = "1432222"
+	s.tel = "09033332222"
+	#s.prefecture_id = 
+	s.address = "名古屋市千種区1-1-1"
+	s.password = "888888"
+	s.nickname = "みっちゃん"
+	#s.cart_id = 
+	#s.doft_destroyed_at = 
+end
+
+User.seed do |s|
+	s.id = 7
+	s.first_name = "綾野"
+	s.last_name = "剛"
+	s.kana_first_name = "アヤノ"
+	s.kana_last_name = "ゴウ"
+	s.email = "momo@gmail.com"
+	s.zipcode = "1432222"
+	s.tel = "09011112222"
+	#s.prefecture_id = 
+	s.address = "世田谷区成城1-1-1"
+	s.password = "888888"
+	s.nickname = "ごう"
+	#s.cart_id = 
+	#s.doft_destroyed_at = 
+end
+
+User.seed do |s|
+	s.id = 8
+	s.first_name = "新沼"
+	s.last_name = "謙治"
+	s.kana_first_name = "ニイヌマ"
+	s.kana_last_name = "ケンジ"
+	s.email = "sushi@gmail.com"
+	s.zipcode = "1598877"
+	s.tel = "09011112222"
+	#s.prefecture_id = 
+	s.address = "秋田市1-1-1"
+	s.password = "888888"
+	s.nickname = "haruka"
+	#s.cart_id = 
+	#s.doft_destroyed_at = 
+end
+
+User.seed do |s|
+	s.id = 9
+	s.first_name = "トリンドル"
+	s.last_name = "玲奈"
+	s.kana_first_name = "トリンドル"
+	s.kana_last_name = "レナ"
+	s.email = "nikuman@gmail.com"
+	s.zipcode = "1432222"
+	s.tel = "09011112222"
+	#s.prefecture_id = 
+	s.address = "富士宮市1-1-1"
+	s.password = "888888"
+	s.nickname = "レナ"
 	#s.cart_id = 
 	#s.doft_destroyed_at = 
 end
