5e33159009bbbe3420dca655a4dd082df0ca31bf
sawaka1991 <sapa61s@gmail.com>
2018-03-21 08:32:09 +0000

    cd edit destroy

--- app/controllers/discs_controller.rb
+++ app/controllers/discs_controller.rb
@@ -2,34 +2,53 @@ class DiscsController < ApplicationController
 
 	def new
 		@cd = Cd.find(params[:cd_id])
-		@disc = Disc.new
-		@song = Song.new
+			@disc = Disc.new
+			@song = Song.new
 	end
 
 	def create
 		@cd = Cd.find(params[:cd_id])
 		@disc = Disc.new(disc_params)
 		@disc.cd_id = @cd.id
-
-
 		if params[:add_disc]
-			@disc.save
-	    	redirect_to new_cd_disc_path(@cd)
-
-  		else params[:create_disc]
-  			@disc.save
-
-	    	redirect_to cds_path
+			if @disc.save
+	    		redirect_to new_cd_disc_path(@cd)
+	    	else
+	    		render :new
+	    	end
+  		elsif params[:create_disc]
+  			if @disc.save
+	    		redirect_to cds_path
+	    	else
+	    		render :new
+	    	end
+		end
 	end
-end
+	def edit
+		@cd = Cd.find(params[:cd_id])
+		@disc = Disc.find(params[:id])
+		@songs = @disc.songs
+
+	end
+	def update
+		@cd = Cd.find(params[:cd_id])
+		@disc = Disc.find(params[:id])
+		@songs = disc.songs
+		@disc.update(disc_params)
+		redirect_to cd_path(cd)
+	end
+
 
 	def destroy
-
+		@cd =Cd.find(params[:cd_id])
+		@disc = Disc.find(params[:id])
+  		@disc.destroy
+  		redirect_to cd_path(@cd)
 	end
 
 private
 def disc_params
-	params.require(:disc).permit(:cd_id, :disc_num, :songs_attributes => [:song_name, :song_kana_name, :order_num, :minutes, :disc_id, :singer_id, :_destroy])
+	params.require(:disc).permit(:cd_id, :disc_num, :songs_attributes => [:id, :song_name, :song_kana_name, :order_num, :minutes, :disc_id, :singer_id, :_destroy])
 
 end
 end
--- app/models/cd.rb
+++ app/models/cd.rb
@@ -4,5 +4,11 @@ class Cd < ApplicationRecord
 	has_many :cd_genres
 	has_many :genres, through: :cd_genres
 	belongs_to :artist
+	has_many :cd_carts
+	has_many :carts, through: :cd_carts
+	belongs_to :user
+end
+
+	validates :album_name, :album_kana_name, :label, :price, :stock, :release_date, :artist_id, presence: true
 
 end
