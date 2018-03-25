fbb78b6bafd3c4d55e311e1f1120d9ed0919fd4e
sawaka1991 <sapa61s@gmail.com>
2018-03-25 11:38:21 +0000

    history,cdhistory

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -5,12 +5,25 @@ class HistoriesController < ApplicationController
 
 	def index
 		@histories = History.all
-    @genres = Genre.all
+    	@genres = Genre.all
 	end
 
 	def show
 		
 	end
+	def create
+		@user = User.find(current_user[:id])
+		@history = History.new(history_params)
+		@history.save
+		@cc = @user.cart.cd_carts
+		@cc.each do |cc|
+			cc.cd.update(stock: cc.cd.stock - cc.count)
+		end
+		@user.cart.cd_carts.delete_all
+		redirect_to cds_path
+	end
+
+
 
 	def destroy
 	end
@@ -21,4 +34,12 @@ class HistoriesController < ApplicationController
 
 	def ensure_correct_user
 	end
+
+private
+
+	def history_params
+		params.require(:history).permit(:total_price, :prefecture_id, :zipcode, :address, :tel, :name, :user_id, :status,
+			cd_histories_attributes: [:id, :history_id, :cd_id, :count, :last_price])
+	end
+
 end
--- app/models/history.rb
+++ app/models/history.rb
@@ -1,9 +1,10 @@
 class History < ApplicationRecord
-
+	attr_accessor :delivery_id
 	enum status:{受付: 0,準備中: 1,発送済: 2}
 
 	has_many :cd_histories
+	accepts_nested_attributes_for :cd_histories
 	has_many :cds, through: :cd_histories
 	belongs_to :user
-	belongs_to :delivery
+
 end
